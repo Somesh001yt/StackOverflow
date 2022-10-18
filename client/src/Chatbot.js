2154784ee04333042c17b98bef9025d3dbabbe8b
Somesh <someshcr7b@gmail.com>
2022-10-18 18:56:21 +0530

    chat bot feature enable

---
+++ client/src/Chatbot.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export class Chatbot extends Component {
+  componentDidMount() {
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "15936c27e097aba8ba6184ae4c6b80c5d",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Chatbot;
